50aac6710508e9df9bcc84b4a88b8bce6bf8a40d
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-07-17 02:02:58 +0000

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {(!props.bio) || (props.bio === "") ? null : <p>{props.bio}</p>}    
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={props.github} linkedin={props.linkedin} />
       {/* add your <Links /> component here */}
     </div>
   );
 }
+
+
 
 export default About;
